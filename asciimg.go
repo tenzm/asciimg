a12edbd428d610aabf12dac045c33505f6a637e2
jnovikov <johnnovikov0@gmail.com>
2020-01-12 14:21:47 +0300

    Task and project update

--- asciimg.go
+++ asciimg.go
@@ -1,73 +1,79 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "image"
-    "image/color"
-    // Side-effect import.
-    // Сайд-эффект — добавление декодера PNG в пакет image.
-    _ "image/png"
-    _ "image/jpeg"
-    "os"
+	"flag"
+	"fmt"
+	"image"
+	"image/color"
+	_ "image/jpeg"
+	// Side-effect import.
+	// Сайд-эффект — добавление декодера PNG в пакет image.
+	_ "image/png"
+	"os"
+	// Внешняя зависимость.
+	"golang.org/x/image/draw"
 )
 
+func scale(img image.Image, w int, h int) image.Image {
+	dstImg := image.NewRGBA(image.Rect(0, 0, w, h))
+	draw.NearestNeighbor.Scale(dstImg, dstImg.Bounds(), img, img.Bounds(), draw.Over, nil)
+	return dstImg
+}
 
 func decodeImageFile(imgName string) (image.Image, error) {
-    imgFile, err := os.Open(imgName)
-    if err != nil {
-        return nil, err
-    }
+	imgFile, err := os.Open(imgName)
+	if err != nil {
+		return nil, err
+	}
 
-    img, _, err := image.Decode(imgFile)
+	img, _, err := image.Decode(imgFile)
 
-    return img, err
+	return img, err
 }
 
 func processPixel(c color.Color) rune {
-    gc := color.GrayModel.Convert(c)
-    r, _, _, _ := gc.RGBA()
-    r = r >> 8
+	gc := color.GrayModel.Convert(c)
+	r, _, _, _ := gc.RGBA()
+	r = r >> 8
 
-    // symbols := []rune("@80GCLft1i;:,. ")
-    return '0'
+	// symbols := []rune("@80GCLft1i;:,. ")
+	return '0'
 }
 
 func convertToAscii(img image.Image) [][]rune {
-    textImg := make([][]rune, img.Bounds().Dy())
-    // i := 0; i < i.Bound().Dy; i++
-    for i := range textImg {
-        textImg[i] = make([]rune, img.Bounds().Dx())
-    }
+	textImg := make([][]rune, img.Bounds().Dy())
+	for i := range textImg {
+		textImg[i] = make([]rune, img.Bounds().Dx())
+	}
 
-    for i := range textImg {
-        for j := range textImg[i] {
-            textImg[i][j] = processPixel(img.At(j, i))
-        }
-    }
-    return textImg
+	for i := range textImg {
+		for j := range textImg[i] {
+			textImg[i][j] = processPixel(img.At(j, i))
+		}
+	}
+	return textImg
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    if flag.NArg() == 0 {
-        fmt.Println("Usage: asciimg <image.jpg>")
-        os.Exit(0)
-    }
-    img := flag.Arg(0)
+	if flag.NArg() == 0 {
+		fmt.Println("Usage: asciimg <image.jpg>")
+		os.Exit(0)
+	}
+	imgName := flag.Arg(0)
 
-    image, err := decodeImageFile(img)
-    if err != nil {
-        fmt.Println("Error:" , err.Error())
-        os.Exit(1)
-    }
+	img, err := decodeImageFile(imgName)
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		os.Exit(1)
+	}
 
-    textImg := convertToAscii(image)
-    for i := range textImg {
-        for j := range textImg[i] {
-            fmt.Printf("%c", textImg[i][j])
-        }
-        fmt.Println()
-    }
+	textImg := convertToAscii(img)
+	for i := range textImg {
+		for j := range textImg[i] {
+			fmt.Printf("%c", textImg[i][j])
+		}
+		fmt.Println()
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module asciimg
 
 go 1.12
+
+require golang.org/x/image v0.0.0-20191214001246-9130b4cfad52
